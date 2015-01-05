d158bab4daa25ddc585c9a813fb961c0218df3a5
Tauseef <tauseef.jamadar@yahoo.com>
2015-01-04 18:31:49 -0800

    Adding jquery mobile css

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" />
 
         <link rel="stylesheet" href="/css/bootstrap.min.css">
         <link rel="stylesheet" href="/css/bootstrap-theme.min.css">
