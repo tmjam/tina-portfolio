dbb95f8d317ca7bd7e398ae5d1987872003d8d23
Tauseef Jamadar <tauseef.jamadar@tjamad0320554.build.internal>
2015-06-29 23:14:55 -0700

    Upgrading PHP version to support google app engine

--- email.php
+++ email.php
@@ -7,12 +7,12 @@ if($_SERVER["REQUEST_METHOD"]=="POST") {
 
    $name = $_POST["name"];
    $email = $_POST["email"];
-   $query = "Hi Kristina, ".$name." with email '".$email."' tried contacting you with the following message <br>" . $_POST["message"];
+   $query = "Hi Kristina, '".$name."' with email '".$email."' tried contacting you with the following message: \n" . $_POST["message"];
 
   require_once 'google/appengine/api/mail/Message.php';
     $mail_options = [
         "sender" => "tmjam.ahmed@gmail.com",
-        "to" => "tmjam.ahmed@gmail.com",
+        "to" => "kfinlayson9@gmail.com",
         "subject" => "Contacting you from portfolio",
         "textBody" => $query,
     ];  
